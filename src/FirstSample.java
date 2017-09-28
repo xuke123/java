faf17f9ca11efc2bdbd536bbd1e45f5a0bd3905b
xuke <1157732469@qq.com>
2017-09-28 21:24:25 +0800

    this is a test

---
+++ src/FirstSample.java
@@ -0,0 +1,9 @@
+public class FirstSample {
+    public static void main(String[] args){
+       // int  a=0b1_000_0000;
+       // System.out.println(a);
+        int a=1278;
+        System.out.printf("%d%<x",a);
+        System.out.println("We will not use 'Hello world'");
+    }
+}
---
+++ src/Retirement.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Retirement {
+    public static void main(String[] args){
+        Scanner in=new Scanner(System.in);
+        System.out.print("How much money do you need to retire?");
+        double goal=in.nextDouble();
+        System.out.println("this is a test");
+    }
+}
